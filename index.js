f9c44f45f98a5fa167d02ba0cec93b1229e6b2c4
sakkarakaya <mehmet-sakib.karakaya@stud.hs-hannover.de>
2021-04-20 17:14:47 +0200

    added redux

--- index.js
+++ index.js
@@ -4,11 +4,15 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+
 // import App from './src/pages/App';
 // import State from './src/pages/State';
 // import Shop from './src/pages/Shop';
-//import Mainshop from './src/pages/Mainshop'
-import Router from './src/Router'
+// import Mainshop from './src/pages/Mainshop'
+// import Router from './src/Router'
+
+import Redux from './src/Redux'
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Router);
+AppRegistry.registerComponent(appName, () => Redux);
---
+++ src/Redux.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, View, Text } from 'react-native'
+
+const Redux = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>rr</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Redux
